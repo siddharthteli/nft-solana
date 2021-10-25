fb1fe5fec9fc584d4da5ee76ceeb6743f84d3c82
siddharth teli <telisiddharth123@gmail.com>
2021-10-25 23:03:08 +0530

    Add:Simple program code . Implemented:
    Because:

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+};
+
+
+
+entrypoint!(process_instruction);
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("program id {:?}", program_id);
+    for account in accounts {
+        msg!("accounts {:?}", account);
+    }
+    msg!("data {:?}", instruction_data);
+    Ok(())
 }

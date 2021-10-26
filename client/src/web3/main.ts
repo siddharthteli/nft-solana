397b424f8a29e80de53701885ad5521efb145919
siddharth teli <telisiddharth123@gmail.com>
2021-10-26 17:00:54 +0530

    Add:Update main method Implemented:
    Because:

--- client/src/web3/main.ts
+++ client/src/web3/main.ts
@@ -1,44 +1,100 @@
-import { Keypair, Transaction, Connection, TransactionInstruction, PublicKey, AccountMeta } from "@solana/web3.js";
+import { Keypair,SystemProgram,sendAndConfirmTransaction, Transaction, Connection, TransactionInstruction, PublicKey, AccountMeta } from "@solana/web3.js";
+import * as borsh from 'borsh';
 
 
 
 
+// export default async function main() {
+//     // 5zwi8GthH6vj3u7uQg9dTsbKSsaeVvseC4nwPLjD4eTy
+// let feePayer = Keypair.fromSecretKey(
+//     Uint8Array.from([183, 24, 219, 158, 145, 227, 59, 153, 68, 209, 188, 54, 95, 47, 59, 131, 223, 143, 101, 3, 35, 208, 224, 128, 111, 66, 113, 241, 221, 62, 215, 202, 74, 70, 182, 171, 100, 140, 55, 222, 82, 240, 81, 41, 113, 181, 17, 113, 99, 181, 56, 144, 64, 245, 12, 189, 86, 144, 95, 175, 105, 33, 29, 136])
+// );
+// // class GreetingAccount {
+// //     counter = 0;
+// //     constructor(fields: {counter: number} | undefined = undefined) {
+// //       if (fields) {
+// //         this.counter = fields.counter;
+// //       }
+// //     }
+// //   }
+  
+// //   const GreetingSchema = new Map([
+// //     [GreetingAccount, {kind: 'struct', fields: [['counter', 'u32']]}],
+// //   ]);
+ 
 
-export default async function main() {
-    // 5zwi8GthH6vj3u7uQg9dTsbKSsaeVvseC4nwPLjD4eTy
-let feePayer = Keypair.fromSecretKey(
-    Uint8Array.from([183, 24, 219, 158, 145, 227, 59, 153, 68, 209, 188, 54, 95, 47, 59, 131, 223, 143, 101, 3, 35, 208, 224, 128, 111, 66, 113, 241, 221, 62, 215, 202, 74, 70, 182, 171, 100, 140, 55, 222, 82, 240, 81, 41, 113, 181, 17, 113, 99, 181, 56, 144, 64, 245, 12, 189, 86, 144, 95, 175, 105, 33, 29, 136])
-);
+// // // your program
+// // let programId = new PublicKey("GoajUKZ1SjGBVu8uxaBcGJmdzbho7UyR9cyWTw3xz84E");
+// //     let connection = new Connection("http://localhost:8899");
+// //     let randomAccount = new Keypair();
+// //     console.log(`random address: ${randomAccount.publicKey.toBase58()}`);
 
-// your program
-let programId = new PublicKey("GoajUKZ1SjGBVu8uxaBcGJmdzbho7UyR9cyWTw3xz84E");
-    let connection = new Connection("http://localhost:8899");
-    let randomAccount = new Keypair();
-    console.log(`random address: ${randomAccount.publicKey.toBase58()}`);
-    let tx = new Transaction();
-    tx.add(
-        new TransactionInstruction({
-            keys: [
-                {
-                    pubkey: feePayer.publicKey,
-                    isSigner: true,
-                    isWritable: true,
-                },
-                {
-                    pubkey: randomAccount.publicKey,
-                    isSigner: false,
-                    isWritable: true,
-                },
-            ], // account meta, program will receive the same order array
-            data: Buffer.from(new Uint8Array([1, 2, 3, 4, 5])), // data
-            programId: programId,
-        })
-    );
-    console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer])}`);
-    // after you sending, you can check tx on official explorer
-    // it should log these data which we pass into tx.
-    // in next chapter I'm going to tell you how to make branch to your program
-}
+
+// //     const GREETING_SEED = 'hello';
+
+// //     let greetedPubkey = await PublicKey.createWithSeed(
+// //       feePayer.publicKey,
+// //       GREETING_SEED,
+// //       programId,
+// //     );
+    
+// //     let greetedPubkey2 = await PublicKey.createWithSeed(
+// //         randomAccount.publicKey,
+// //         GREETING_SEED,
+// //         programId,
+// //       );
+// //       const GREETING_SIZE = borsh.serialize(
+// //         GreetingSchema,
+// //         new GreetingAccount(),
+// //       ).length;
+      
+
+// //       const greetedAccount = await connection.getAccountInfo(greetedPubkey);
+// //       if (greetedAccount === null) {
+// //         console.log(
+// //           'Creating account',
+// //           greetedPubkey.toBase58(),
+// //           'to say hello to',
+// //         );
+// //         const lamports = await connection.getMinimumBalanceForRentExemption(
+// //           GREETING_SIZE,
+// //         );
+    
+// //         const transaction = new Transaction().add(
+// //           SystemProgram.createAccountWithSeed({
+// //             fromPubkey: feePayer.publicKey,
+// //             basePubkey: feePayer.publicKey,
+// //             seed: GREETING_SEED,
+// //             newAccountPubkey: greetedPubkey,
+// //             lamports,
+// //             space: GREETING_SIZE,
+// //             programId,
+// //           }),
+// //         );
+// //         await sendAndConfirmTransaction(connection, transaction, [feePayer]);
+// //       }
+// //       let tx = new Transaction();
+// //       tx.add(
+// //           new TransactionInstruction({
+// //               keys: [
+// //                   {
+// //                       pubkey: greetedPubkey,
+// //                       isSigner: false,
+// //                       isWritable: true,
+// //                   },
+// //                   {
+// //                       pubkey: greetedPubkey2,
+// //                       isSigner: false,
+// //                       isWritable: true,
+// //                   },
+// //               ], // account meta, program will receive the same order array
+// //               data: Buffer.from(new Uint8Array([1, 2, 3, 4, 5])), // data
+// //               programId: programId,
+// //           })
+// //       );
+// //       console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer])}`);
+//     }
+
 
 export async function invoke() {
     main().then(
@@ -50,3 +106,78 @@ export async function invoke() {
 
 }
 
+
+
+async function main() {
+    class GreetingAccount {
+        counter = 0;
+        constructor(fields: {counter: number} | undefined = undefined) {
+          if (fields) {
+            this.counter = fields.counter;
+          }
+        }
+      }
+      const GreetingSchema = new Map([
+        [GreetingAccount, {kind: 'struct', fields: [['counter', 'u32']]}],
+      ]);
+      const GREETING_SIZE = borsh.serialize(
+        GreetingSchema,
+        new GreetingAccount(),
+      ).length;
+
+      //
+    let feePayer = Keypair.fromSecretKey(
+             Uint8Array.from([183, 24, 219, 158, 145, 227, 59, 153, 68, 209, 188, 54, 95, 47, 59, 131, 223, 143, 101, 3, 35, 208, 224, 128, 111, 66, 113, 241, 221, 62, 215, 202, 74, 70, 182, 171, 100, 140, 55, 222, 82, 240, 81, 41, 113, 181, 17, 113, 99, 181, 56, 144, 64, 245, 12, 189, 86, 144, 95, 175, 105, 33, 29, 136]));
+    let connection = new Connection("http://localhost:8899");
+    let programId = new PublicKey("GoajUKZ1SjGBVu8uxaBcGJmdzbho7UyR9cyWTw3xz84E");
+    let randomAccount = new Keypair();
+    const GREETING_SEED = 'hello';
+ 
+    const greetedPubkey = await PublicKey.createWithSeed(
+        feePayer.publicKey,
+    GREETING_SEED,
+    programId,
+  );
+ 
+  const lamports = await connection.getMinimumBalanceForRentExemption(
+    GREETING_SIZE,
+  );
+
+  const transaction = new Transaction().add(
+    SystemProgram.createAccountWithSeed({
+      fromPubkey: feePayer.publicKey,
+      basePubkey: feePayer.publicKey,
+      seed: GREETING_SEED,
+      newAccountPubkey: greetedPubkey,
+      lamports,
+      space: GREETING_SIZE,
+      programId,
+    }),
+  );
+  const hash = await sendAndConfirmTransaction(connection, transaction, [
+    feePayer,
+  ]);
+    console.log(`random address: ${randomAccount.publicKey.toBase58()}`);
+  
+    let tx = new Transaction();
+    tx.add(
+      new TransactionInstruction({
+        keys: [
+          {
+            pubkey: feePayer.publicKey,
+            isSigner: true,
+            isWritable: true,
+          },
+          {
+            pubkey: randomAccount.publicKey,
+            isSigner: false,
+            isWritable: true,
+          },
+        ], // account meta, program will receive the same order array
+        data: Buffer.from(new Uint8Array([1, 2, 3, 4, 5])), // data
+        programId: programId,
+      })
+    );
+    console.log(`txhash: ${await connection.sendTransaction(tx, [feePayer])}`);
+
+  }
--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,13 +1,58 @@
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+    program_error::ProgramError,
+    account_info::{AccountInfo,next_account_info}, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
-
+use std::cell::RefCell;
+use std::rc::Rc;
+use borsh::{BorshDeserialize, BorshSerialize};
 //after solana-keygen new
 //5zwi8GthH6vj3u7uQg9dTsbKSsaeVvseC4nwPLjD4eTy
 //[183,24,219,158,145,227,59,153,68,209,188,54,95,47,59,131,223,143,101,3,35,208,224,128,111,66,113,241,221,62,215,202,74,70,182,171,100,140,55,222,82,240,81,41,113,181,17,113,99,181,56,144,64,245,12,189,86,144,95,175,105,33,29,136]
 // program id-
 //GoajUKZ1SjGBVu8uxaBcGJmdzbho7UyR9cyWTw3xz84E
-entrypoint!(process_instruction);
+
+//build program -
+//cargo build-bpf --manifest-path=program/Cargo.toml --bpf-out-dir=dist/program
+// deploy -
+//solana program deploy /home/siddharth/WowLabz/Solana/nft-solana/dist/program/helloworld.so
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct GreetingAccount {
+    pub counter: u32,
+    
+}
+ entrypoint!(process_instruction);
+// fn process_instruction(
+//     program_id: &Pubkey,
+//     accounts: &[AccountInfo],
+//     instruction_data: &[u8],
+// ) -> ProgramResult {
+//     msg!("program id {:?}", program_id);
+//     let accounts_iter = &mut accounts.iter();
+//     let account = next_account_info(accounts_iter)?;
+//         msg!("accounts {:?}", account.data.borrow());
+//         if account.owner != program_id {
+//             msg!("Greeted account does not have the correct program id");
+//             return Err(ProgramError::IncorrectProgramId);
+//         }
+//     // let mut greeting_account = GreetingAccount::try_from_slice(&account.data.borrow())?;
+//     // greeting_account.counter += 1;
+//     // greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
+//     // msg!("Greeted {} time(s)!", greeting_account.counter);
+//     // msg!("Testtime(s)!");
+// //     let my_ref=Rc::new(RefCell::new(& mut vec![2]));
+// //    let container= account.data.borrow_mut();
+// //    container.
+//   // let test= my_ref.borrow_mut();
+   
+//    //stest.take();
+//    //test.borrow();
+//   //  *account.data= &mut Rc::new(RefCell::new(&mut  vec![45]));   
+    
+//     msg!("data {:?}", instruction_data);
+//     Ok(())
+// }
+
+
 fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
